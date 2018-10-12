849cca41ed03851bacfc5013592282951744f70a
Ray <rayonishi11@gmail.com>
2018-10-12 16:15:45 -0400

    Started work on adding second parametric

--- sketch.js
+++ sketch.js
@@ -1,32 +1,43 @@
 var interval = 0.05;
 var a = 100;
-var prev = [];
-var x;
-var y;
-var v;
+var prev1 = [];
+
+var x1;
+var y1;
+var v1; // Vector for first parametric
+var xequ1;
+var yequ1;
+var xinput1;
+var yinput1;
+
+var x2;
+var y2;
+var v2; // Vector for first parametric
+var xequ2;
+var yequ2;
+var xinput2;
+var yinput2;
+
 var t = 0;
-var xequ;
-var yequ;
+
 var submit;
-var xinput;
-var yinput;
 var reset;
-var error;
+// var error;
 
 function setup(){
-  createCanvas(500, 500);
+  createCanvas(600, 600);
   frameRate(60);
   createP("First box is for x, second is for y. You can input sin/cos as sin(t) or cos(t)");
   createP("You must use * for multiplication, e.g. sin(2t) must be formatted as sin(2 * t)");
   xinput = createInput();
   yinput = createInput();
-  submit = createButton('submit');
-  submit.mousePressed(para);
+  submit1 = createButton('submit');
+  submit1.mousePressed(para);
 }
 
 function para(){
-  xequ = xinput.value();
-  yequ = yinput.value();
+  xequ1 = xinput.value();
+  yequ1 = yinput.value();
 }
 
 function draw(){
@@ -38,28 +49,28 @@ function draw(){
   //y = a * sin(t) * (1-cos(t));
   t+=interval;
   try{
-    eval(xequ);
-    eval(yequ);
+    eval(xequ1);
+    eval(yequ1);
   }
   catch(err){
     error=true
   }
   if(!error) {
-    var tempx = eval(xequ);
-    x = a * tempx;
-    var tempy = eval(yequ);
-    y = a * tempy;
+    var tempx = eval(xequ1);
+    x1 = a * tempx;
+    var tempy = eval(yequ1);
+    y1 = a * tempy;
   }
-  v = createVector(x,y);
-  ellipse(v.x,v.y,10,10)
-  prev.push(v);
+  v1 = createVector(x1,y1);
+  ellipse(v1.x,v1.y,10,10)
+  prev1.push(v1);
 
-  if(prev.length > 100) {
-    prev.splice(0,1);
+  if(prev1.length > 100) {
+    prev1.splice(0,1);
   }
 
-  for(var i = 0; i < prev.length; i++){
-    var pos = prev[i];
+  for(var i = 0; i < prev1.length; i++){
+    var pos = prev1[i];
     fill(255,0,0);
     ellipse(pos.x,pos.y,1,1);
   }
